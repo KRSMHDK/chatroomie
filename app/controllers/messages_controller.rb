ead21f2cc590d143e493a654db27cabb286acd83
Kris Mahadhika <krismahadhika@gmail.com>
2023-05-07 17:13:25 +0800

    redirect when form send also add permit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,17 @@
 class MessagesController < ApplicationController
   def create
     user = User.find(current_user.id)
-    @message = user.messages.create!(content: params[:content], room_id: params[:room_id])
-    # ActionCable.server.broadcast "chatroom_channel", message: render_message(@message)
+    @message = user.messages.create!(content: msg_params[:content], room_id: params[:room_id])
+
+    if @message.save
+      redirect_to chatroom_path(@message.room), status: :see_other
+    else
+      render :new
+    end
   end
 
   private
-  def render_message(message)
-    ApplicationController.renderer.render(partial: "messages/message", locals: { message: message })
+  def msg_params
+    params.permit(:content)
   end
-
 end
