8fdcfc9648d75b45afa7fe358bb2c4f2c346eb21
Kris Mahadhika <krismahadhika@gmail.com>
2023-05-07 17:11:47 +0800

    remove whitespace

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user =  User.find_or_create_by(name: params[:name])
+    user = User.find_or_create_by(name: params[:name])
 
     if user
       log_in(user, params[:room])
