f46cf65c06a75d13d62a781f0c41fc33d3714baf
Kris Mahadhika <krismahadhika@gmail.com>
2023-05-07 23:52:35 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
-  delete '/signout', to: 'sessions#destroy'
+  delete '/signout', to: 'sessions#destroy', as: 'signout'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
   resources :messages, only: [:create]
   resources :chatroom, only: [:show]
-  resources :sessions, only: [:new, :create, :destroy]
-
+  resources :sessions, only: [:new, :create]
   root "users#index"
   # Defines the root path route ("/")
 end
