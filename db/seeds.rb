d4dbfac0631c2f123f40dae11d281506d00c573b
Kris Mahadhika <krismahadhika@gmail.com>
2023-05-07 19:16:17 +0800

    align pic to center

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Room.create(name: 'Canary')
+Room.create(name: 'Toucan')
+Room.create(name: 'Macaw')
