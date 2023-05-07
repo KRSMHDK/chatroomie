6fd2f85065d81e0b14e528369d579d18e5ba955f
Kris Mahadhika <krismahadhika@gmail.com>
2023-05-07 23:52:05 +0800

    redirect if user not loggedin

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,11 @@
 class ChatroomController < ApplicationController
   def show
+    if !current_user
+      redirect_to root_path
+    end
+
     @users = User.all
+    @rooms = Room.all
     @room = Room.find(params[:id])
     @messages = Message.where(room_id: params[:id])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    if current_user
+      redirect_to chatroom_path(1)
+    end
     @room = Room.all
   end
 end
