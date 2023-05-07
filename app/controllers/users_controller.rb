6af341feb7ae4383411443d809215bcf62a312e6
Kris Mahadhika <krismahadhika@gmail.com>
2023-05-07 11:09:50 +0800

    unused code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,4 @@ class UsersController < ApplicationController
   def index
     @room = Room.all
   end
-
-  def create
-    user = User.find_or_create_by(name: params[:name])
-
-    if user
-      session[:user] = user
-      redirect_to chatroom_path(params[:room])
-    end
-  end
 end
